af2b91e5e12392bffab2c5d3e7f6d2671067ccf3
Jukka Lehtosalo <jukka.lehtosalo@iki.fi>
2025-12-29 15:08:04 +0000

    Use Vec<u8> instead of Writer

--- src/serialize_ast.rs
+++ src/serialize_ast.rs
@@ -78,7 +78,7 @@ pub(crate) fn main(args: &Args) -> Result<()> {
     let mut v = Vec::new();
     let line_index = LineIndex::from_source_text(source_kind.source_code());
     let mut state = State { imports: Vec::new() };
-    python_ast.serialize(&mut v, &mut state, &line_index, source_kind.source_code()).unwrap();
+    python_ast.serialize(&mut v, &mut state, &line_index, source_kind.source_code());
 
     io::stdout().write_all(&v)?;
 
@@ -96,54 +96,53 @@ struct State {
 }
 
 trait Ser {
-    fn serialize<W: Write>(&self, w: &mut W, state: &mut State, l: &LineIndex, text: &str) -> io::Result<()>;
+    fn serialize(&self, w: &mut Vec<u8>, state: &mut State, l: &LineIndex, text: &str);
 }
 
 impl Ser for ast::Mod {
-    fn serialize<W: Write>(&self, w: &mut W, state: &mut State, l: &LineIndex, text: &str) -> io::Result<()> {
+    fn serialize(&self, w: &mut Vec<u8>, state: &mut State, l: &LineIndex, text: &str) {
         match self {
             ast::Mod::Module(m) => {
-                write_tagged_int(w, m.body.len() as i64)?;
+                write_tagged_int(w, m.body.len() as i64);
                 for stmt in &m.body {
-                    stmt.serialize(w, state, l, text)?;
+                    stmt.serialize(w, state, l, text);
                 }
             }
             ast::Mod::Expression(_) => {
                 panic!("Expression unsupported");
             }
         }
-        Ok(())
     }
 }
 
 impl Ser for ast::Stmt {
-    fn serialize<W: Write>(&self, w: &mut W, state: &mut State, l: &LineIndex, text: &str) -> io::Result<()> {
+    fn serialize(&self, w: &mut Vec<u8>, state: &mut State, l: &LineIndex, text: &str) {
         match self {
             ast::Stmt::Expr(e) => {
-                write_tag(w, TAG_EXPR_STMT)?;
-                e.value.serialize(w, state, l, text)?;
+                write_tag(w, TAG_EXPR_STMT);
+                e.value.serialize(w, state, l, text);
             }
             ast::Stmt::Import(i) => {
-                write_tag(w, TAG_IMPORT)?;
+                write_tag(w, TAG_IMPORT);
                 for name in &i.names {
-                    write_bytes(w, name.name.as_bytes())?;
+                    write_bytes(w, name.name.as_bytes());
                     state.imports.push(Import { name: name.name.to_string(), relative: 0, as_name: None});
                 }
-                write_location(w, l, text, i.range())?;
+                write_location(w, l, text, i.range());
             }
             ast::Stmt::If(s) => {
-                write_tag(w, TAG_IF)?;
-                s.test.serialize(w, state, l, text)?;
-                serialize_block(w, state, l, text, &s.body)?;
-                write_usize(w, s.elif_else_clauses.len())?;
+                write_tag(w, TAG_IF);
+                s.test.serialize(w, state, l, text);
+                serialize_block(w, state, l, text, &s.body);
+                write_usize(w, s.elif_else_clauses.len());
                 for ee in &s.elif_else_clauses {
                     match &ee.test {
                         Some(e) => {
-                            e.serialize(w, state, l, text)?;
-                            serialize_block(w, state, l, text, &ee.body)?;
+                            e.serialize(w, state, l, text);
+                            serialize_block(w, state, l, text, &ee.body);
                         }
                         None => {
-                            serialize_block(w, state, l, text, &ee.body)?;
+                            serialize_block(w, state, l, text, &ee.body);
                         }
                     }
                 }
@@ -157,59 +156,59 @@ impl Ser for ast::Stmt {
 }
 
 impl Ser for ast::Expr {
-    fn serialize<W: Write>(&self, w: &mut W, state: &mut State, l: &LineIndex, text: &str) -> io::Result<()> {
-        let write_loc = |w: &mut W, r: TextRange| write_location(w, l, text, r);
+    fn serialize(&self, w: &mut Vec<u8>, state: &mut State, l: &LineIndex, text: &str) {
+        let write_loc = |w: &mut Vec<u8>, r: TextRange| write_location(w, l, text, r);
 
         match self {
             ast::Expr::Name(n) => {
-                write_tag(w, TAG_NAME_EXPR)?;
-                write_bytes(w, n.id.as_bytes())?;
-                write_loc(w, n.range())?;
+                write_tag(w, TAG_NAME_EXPR);
+                write_bytes(w, n.id.as_bytes());
+                write_loc(w, n.range());
             }
             ast::Expr::Attribute(a) => {
-                write_tag(w, TAG_MEMBER_EXPR)?;
-                a.value.serialize(w, state, l, text)?;
-                write_bytes(w, a.attr.as_bytes())?;
-                write_loc(w, a.range())?;
+                write_tag(w, TAG_MEMBER_EXPR);
+                a.value.serialize(w, state, l, text);
+                write_bytes(w, a.attr.as_bytes());
+                write_loc(w, a.range());
             }
             ast::Expr::StringLiteral(s) => {
-                write_tag(w, TAG_STR_EXPR)?;
+                write_tag(w, TAG_STR_EXPR);
                 let value = &s.value;
-                write_tag(w, TAG_LITERAL_STR)?;
-                write_usize(w, value.len())?;
+                write_tag(w, TAG_LITERAL_STR);
+                write_usize(w, value.len());
                 for part in value.iter() {
-                    w.write(part.as_bytes())?;
+                    w.extend_from_slice(part.as_bytes());
                 }
-                write_loc(w, s.range())?;
+                write_loc(w, s.range());
             }
             ast::Expr::Call(c) => {
-                write_tag(w, TAG_CALL_EXPR)?;
-                c.func.serialize(w, state, l, text)?;
+                write_tag(w, TAG_CALL_EXPR);
+                c.func.serialize(w, state, l, text);
                 let args = &c.arguments;
-                write_tag(w, TAG_LIST_GEN)?;
-                write_int(w, args.len() as i64)?;
+                write_tag(w, TAG_LIST_GEN);
+                write_int(w, args.len() as i64);
                 for arg in &args.args {
-                    arg.serialize(w, state, l, text)?;
+                    arg.serialize(w, state, l, text);
                 }
                 if args.keywords.len() > 0 {
                     // TODO: Keywords
                     panic!("unsupported: {:?}", args.keywords);
                 }
-                write_loc(w, c.range())?;
+                write_loc(w, c.range());
             }
             ast::Expr::BinOp(b) => {
-                write_tag(w, TAG_OP_EXPR)?;
-                w.write_all(&[b.op as u8])?;
-                b.left.serialize(w, state, l, text)?;
-                b.right.serialize(w, state, l, text)?;
+                write_tag(w, TAG_OP_EXPR);
+                w.push(b.op as u8);
+                b.left.serialize(w, state, l, text);
+                b.right.serialize(w, state, l, text);
             }
             ast::Expr::NumberLiteral(n) => {
                 match &n.value {
                     Number::Int(n) => {
                         match n.as_i64() {
                             Some(x) => {
-                                write_tag(w, TAG_INT_EXPR)?;
-                                write_tagged_int(w, x)?;
+                                write_tag(w, TAG_INT_EXPR);
+                                write_tagged_int(w, x);
                             }
                             _ => {
                                 panic!("unsupported big int: {self:?}");
@@ -229,37 +228,36 @@ impl Ser for ast::Expr {
     }
 }
 
-fn serialize_block(w: &mut impl Write, state: &mut State, l: &LineIndex, text: &str, block: &Vec<ast::Stmt>) -> io::Result<()> {
-    write_tag(w, TAG_BLOCK)?;
-    write_usize(w, block.len())?;
+fn serialize_block(w: &mut Vec<u8>, state: &mut State, l: &LineIndex, text: &str, block: &Vec<ast::Stmt>) {
+    write_tag(w, TAG_BLOCK);
+    write_usize(w, block.len());
     for stmt in block {
-        stmt.serialize(w, state, l, text)?;
+        stmt.serialize(w, state, l, text);
     }
-    write_end_tag(w)?;
-    Ok(())
+    write_end_tag(w);
 }
 
 #[inline]
-fn write_tagged_int(w: &mut impl Write, i: i64) -> io::Result<()> {
-    write_tag(w, TAG_LITERAL_INT)?;
-    write_int(w, i)
+fn write_tagged_int(w: &mut Vec<u8>, i: i64) {
+    write_tag(w, TAG_LITERAL_INT);
+    write_int(w, i);
 }
 
-fn write_int(w: &mut impl Write, i: i64) -> io::Result<()> {
+fn write_int(w: &mut Vec<u8>, i: i64) {
     if i >= MIN_SHORT_INT && i < 128 + MIN_SHORT_INT {
         // 1-byte format
-        w.write_all(&[((i - MIN_SHORT_INT) << 1) as u8])
+        w.push(((i - MIN_SHORT_INT) << 1) as u8);
     } else if i >= MIN_TWO_BYTES_INT && i <= MAX_TWO_BYTES_INT {
         // 2-byte format
         let x: u16 = (((i - MIN_TWO_BYTES_INT) << 2) | TWO_BYTES_INT_BIT) as u16;
-        w.write_all(&x.to_le_bytes())
+        w.extend_from_slice(&x.to_le_bytes());
     } else if i >= MIN_FOUR_BYTES_INT && i <= MAX_FOUR_BYTES_INT {
         // 4-byte format
         let x: u32 = (((i - MIN_FOUR_BYTES_INT) << 3) | FOUR_BYTES_INT_TRAILER) as u32;
-        w.write_all(&x.to_le_bytes())
+        w.extend_from_slice(&x.to_le_bytes());
     } else {
         // Variable-length format
-        w.write_all(&[LONG_INT_TRAILER])?;
+        w.push(LONG_INT_TRAILER);
         let neg = i < 0;
         let absval = if neg { i.wrapping_abs() as u64 } else { i as u64 };
         let bytes = absval.to_le_bytes();
@@ -267,43 +265,42 @@ fn write_int(w: &mut impl Write, i: i64) -> io::Result<()> {
         while n > 1 && bytes[n - 1] == 0 {
             n -= 1;
         }
-        write_int(w, ((n as i64) << 1) | (neg as i64))?;
-        w.write_all(&bytes[..n])
+        write_int(w, ((n as i64) << 1) | (neg as i64));
+        w.extend_from_slice(&bytes[..n]);
     }
 }
 
 #[inline]
-fn write_tag(w: &mut impl Write, i: u8) -> io::Result<()> {
-    w.write_all(&[i])
-
+fn write_tag(w: &mut Vec<u8>, i: u8) {
+    w.push(i);
 }
 
 #[inline]
-fn write_end_tag(w: &mut impl Write) -> io::Result<()> {
-    write_tag(w, TAG_END)
+fn write_end_tag(w: &mut Vec<u8>) {
+    write_tag(w, TAG_END);
 }
 
 #[inline]
-fn write_usize(w: &mut impl Write, i: usize) -> io::Result<()> {
-    write_int(w, i as i64)
+fn write_usize(w: &mut Vec<u8>, i: usize) {
+    write_int(w, i as i64);
 }
 
-fn write_bytes(w: &mut impl Write, b: &[u8]) -> io::Result<()> {
-    write_tag(w, TAG_LITERAL_STR)?;
-    write_usize(w, b.len())?;
-    w.write_all(b)
+fn write_bytes(w: &mut Vec<u8>, b: &[u8]) {
+    write_tag(w, TAG_LITERAL_STR);
+    write_usize(w, b.len());
+    w.extend_from_slice(b);
 }
 
-fn write_location<W: Write>(w: &mut W, l: &LineIndex, text: &str, range: TextRange) -> io::Result<()> {
-    write_tag(w, TAG_LOCATION)?;
+fn write_location(w: &mut Vec<u8>, l: &LineIndex, text: &str, range: TextRange) {
+    write_tag(w, TAG_LOCATION);
     let st_loc = l.line_column(range.start(), text);
     let st_line = st_loc.line.get() as i64;
     let st_column = st_loc.column.get() as i64;
-    write_int(w, st_line)?;
-    write_int(w, st_column)?;
+    write_int(w, st_line);
+    write_int(w, st_column);
     let end_loc = l.line_column(range.end(), text);
-    write_int(w, (end_loc.line.get() as i64) - st_line)?;
-    write_int(w, (end_loc.column.get() as i64) - st_column)
+    write_int(w, (end_loc.line.get() as i64) - st_line);
+    write_int(w, (end_loc.column.get() as i64) - st_column);
 }
 
 #[cfg(test)]
@@ -318,7 +315,7 @@ mod tests {
     fn test_write_short_int() {
         for x in [-10, -1, 0, 1, 117] {
             let mut v: Vec<u8> = Vec::new();
-            write_int(&mut v, x).unwrap();
+            write_int(&mut v, x);
             assert_eq!(v, &[((x - MIN_SHORT_INT) << 1) as u8]);
         }
     }
@@ -326,49 +323,49 @@ mod tests {
     #[test]
     fn test_write_2_byte_int() {
         let mut v: Vec<u8> = Vec::new();
-        write_int(&mut v, 118).unwrap();
+        write_int(&mut v, 118);
         assert_eq!(v, &[105, 3]);
 
         let mut v: Vec<u8> = Vec::new();
-        write_int(&mut v, -11).unwrap();
+        write_int(&mut v, -11);
         assert_eq!(v, &[101, 1]);
 
         let mut v: Vec<u8> = Vec::new();
-        write_int(&mut v, -100).unwrap();
+        write_int(&mut v, -100);
         assert_eq!(v, &[1, 0]);
 
         let mut v: Vec<u8> = Vec::new();
-        write_int(&mut v, 16283).unwrap();
+        write_int(&mut v, 16283);
         assert_eq!(v, &[253, 255]);
     }
 
     #[test]
     fn test_write_4_byte_int() {
         let mut v: Vec<u8> = Vec::new();
-        write_int(&mut v, -101).unwrap();
+        write_int(&mut v, -101);
         assert_eq!(v, &[91, 53, 1, 0]);
 
         let mut v: Vec<u8> = Vec::new();
-        write_int(&mut v, 16284).unwrap();
+        write_int(&mut v, 16284);
         assert_eq!(v, &[99, 53, 3, 0]);
 
         let mut v: Vec<u8> = Vec::new();
-        write_int(&mut v, -10000).unwrap();
+        write_int(&mut v, -10000);
         assert_eq!(v, &[3, 0, 0, 0]);
 
         let mut v: Vec<u8> = Vec::new();
-        write_int(&mut v, 536860911).unwrap();
-        assert_eq!(v, &[251, 255, 255, 255]);    
+        write_int(&mut v, 536860911);
+        assert_eq!(v, &[251, 255, 255, 255]);
     }
 
     #[test]
     fn test_write_long_int() {
         let mut v: Vec<u8> = Vec::new();
-        write_int(&mut v, -10001).unwrap();
+        write_int(&mut v, -10001);
         assert_eq!(v, &[15, 30, 17, 39]);
 
         let mut v: Vec<u8> = Vec::new();
-        write_int(&mut v, 536860912).unwrap();
+        write_int(&mut v, 536860912);
         assert_eq!(v, &[15, 36, 240, 216, 255, 31]);
     }
 
@@ -380,7 +377,7 @@ mod tests {
         let mut v = Vec::new();
         let index = LineIndex::from_source_text(text);
         let mut state = State { imports: Vec::new() };
-        ast.serialize(&mut v, &mut state, &index, text).unwrap();
+        ast.serialize(&mut v, &mut state, &index, text);
         let _ = state;  // TODO: drop when not needed
 
         let expected = &[
